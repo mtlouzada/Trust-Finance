e83e5bf6dff66cf50a34d38e69ac1bb8b8f045ba
mt.louzada <matheuslouzadaa@gmail.com>
2025-04-28 16:03:56 -0300

    feat: created ViewModels folder and started configuring the initial file

---
+++ ViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,8 @@
+namespace TF.ViewModels
+{
+    public class CreateCategoryViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+    }
+}
