3f66e0ad7f8e14ce5a02e508036fb47b14b254f4
mt.louzada <matheuslouzadaa@gmail.com>
2025-04-29 17:06:10 -0300

    chore: updated migrations by removing a column from the User table

---
+++ Migrations/20250429174106_RemoveBioFromUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trust_Finance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBioFromUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
