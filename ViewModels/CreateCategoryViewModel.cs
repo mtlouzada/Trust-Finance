762342db69f2ecfcbda686d75711d9c36ca6b21b
mtlouzada <matheuslouzadaa@gmail.com>
2025-04-29 07:15:29 -0300

    feat: finalized views for CategoryController

--- ViewModels/CreateCategoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TF.ViewModels
-{
-    public class CreateCategoryViewModel
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Slug { get; set; } = string.Empty;
-    }
-}
