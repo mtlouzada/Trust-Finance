30fb99a521fddcac1e4253c2c8fd52c1a25cf227
mtlouzada <matheuslouzadaa@gmail.com>
2025-04-30 22:12:14 -0300

    feat: created controller to apply token dependency injection

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+namespace TF.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using TF.Data;
+    using TF.Models;
+    using TF.Services;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly TFDataContext _context;
+        private readonly TokenServices _tokenServices;
+        public AccountController(TFDataContext context, TokenServices tokenServices)
+        {
+            _context = context;
+            _tokenServices = tokenServices;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (await UserExists(user.UserName))
+                return BadRequest("User already exists");
+
+            var passwordHash = new PasswordHasher<User>();
+            user.PasswordHash = passwordHash.HashPassword(user, user.PasswordHash);
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+            return Ok(new { token = _tokenServices.GenerateToken(user) });
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
